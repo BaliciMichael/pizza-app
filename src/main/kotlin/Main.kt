8a4ee2d90d89a76d60df10eaebdda4fcb27920ea
BaliciMichael <balicimichael@gmail.com>
2022-04-25 14:31:47 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+import utils.ScannerInput
+import utils.ScannerInput.readNextInt
+import utils.ScannerInput.readNextLine
+import java.io.File
+import java.lang.System.exit
+
---
+++ src/main/kotlin/models/Pizza.kt
@@ -0,0 +1,13 @@
+package models
+
+data class Pizza(var PizzaTitle: String,
+                 var PizzaPrice: Int,
+                 var PizzaType: String,
+                 var ToppingsAvailable: Boolean,
+
+
+
+
+                 ) {
+
+}
